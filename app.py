87b33d0982f57c06de00566994c30a415937f8f7
T5o2 <Tadzcl@gmail.com>
2026-09-23 17:06:05 +0300

    Fix indentation for update_ui function definition

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def analyze_pixels(frame):
     density = min(int((total_crowd / roi_limit) * 100), 100)
     empty = 100 - density
     
-   def update_ui(density, empty, m_r, w_r, placeholders):
+def update_ui(density, empty, m_r, w_r, placeholders):
        
     card_red = generate_card(alert_bg, "نسبة الازدحام", density, "#ff4b4b")
     card_green = generate_card(safe_bg, "نسبة الفضاوة", empty, "#00fa9a")
